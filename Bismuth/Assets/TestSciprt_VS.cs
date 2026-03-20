743a81855160f6de660fbbe109188a72515e4004
Hooni0_0 <startrack0515@gmail.com>
2026-03-20 20:49:23 +0900

    [Create] Bismuth 프로젝트 생성
    - 한글 인코딩 테스트 용 스크립트 추가
    - .gitignore 추가
    - 인코딩 config 추가

---
+++ Bismuth/Assets/TestSciprt_VS.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class TestSciprt_VS : MonoBehaviour
+{
+    // 테스트 한글 테스트
+    // 가난다라마ㅏ바사사ㅏ카차ㅏ자차ㅏ파하ㅏㅎ
+
+}
---
+++ Bismuth/Assets/TestScript_Rider.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    // 테스트 한글 테스트 한글
+    // 가나다ㅏ마바사아아ㅏㅇ아아ㅏ아
+}
