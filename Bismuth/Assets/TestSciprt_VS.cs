60f799616696cfd28797db1726405c2e4fdf13ad
Hooni0_0 <startrack0515@gmail.com>
2026-03-23 11:30:48 +0900

    [test] 테스트 코드 삭제 260323
    - VS/Rider 한글 인코딩 테스트 용 코드

--- Bismuth/Assets/TestSciprt_VS.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class TestSciprt_VS : MonoBehaviour
-{
-    // 테스트 한글 테스트
-    // 가난다라마ㅏ바사사ㅏ카차ㅏ자차ㅏ파하ㅏㅎ
-
-}
--- Bismuth/Assets/TestScript_Rider.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class TestScripts : MonoBehaviour
-{
-    // 테스트 한글 테스트 한글
-    // 가나다ㅏ마바사아아ㅏㅇ아아ㅏ아
-}
